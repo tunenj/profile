f910643da1a513e3b1e1fa3837cefdbb0b401d5a
tunenj <yakubyusuf6@gmail.com>
2024-08-31 18:54:19 +0100

    first commit

---
+++ react-project/src/components/Hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Yusuf Yakub</h1>
+        <p className={styles.description}>
+          I'm a Frontend  developer using React,
+          <br />
+          in creating user-friendly, responsive, and visually appealing web applications.
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/my_img.jpg")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
